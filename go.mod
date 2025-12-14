380a9193b3d9303016c1a0ec53b2b093c053724a
Tarun <tarunsinghkanyal@gmail.com>
2025-12-15 00:35:40 +0530

    First Update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GitUserAct
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Reset  = "\033[0m"
+)
+
+type Event struct {
+	Type string `json:"type"`
+	Repo struct {
+		Name string `json:"name"`
+	} `json:"repo"`
+	Payload struct {
+		Action  string                   `json:"action"`
+		Commits []map[string]interface{} `json:"commits"`
+	} `json:"payload"`
+}
+
+func eventFormatter(e Event) string {
+	switch e.Type {
+	case "PushEvent":
+		return pushEvent(e)
+	case "WatchEvent":
+		return watchEvent(e)
+	case "IssuesEvent":
+		return issuesEvent(e)
+	case "ForkEvent":
+		return forkEvent(e)
+	default:
+		return ""
+	}
+}
+
+func pushEvent(e Event) string {
+	if len(e.Payload.Commits) == 0 {
+		return ""
+	}
+	res := fmt.Sprintf("Pushed %s%v commits%s to %s", Blue, len(e.Payload.Commits), Reset, e.Repo.Name)
+	return res
+}
+func watchEvent(e Event) string {
+	res := fmt.Sprintf("%sStarred%s %s", Green, Reset, e.Repo.Name)
+	return res
+}
+func issuesEvent(e Event) string {
+	actionName := e.Payload.Action
+	actionName = fmt.Sprint(strings.ToUpper(actionName[:1]), actionName[1:])
+
+	res := fmt.Sprintf("%s%s%s an issue in %s", Yellow, actionName, Reset, e.Repo.Name)
+	return res
+}
+func forkEvent(e Event) string {
+	res := fmt.Sprintf("%sForked%s %s", Green, Reset, e.Repo.Name)
+	return res
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Pls Enter Username")
+		return
+	}
+	userName := os.Args[1]
+
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", userName)
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Network error:", err)
+		return
+	}
+
+	defer response.Body.Close()
+	if response.StatusCode == http.StatusNotFound {
+		fmt.Println("Invalid User")
+		return
+	}
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("Unexpected status:", response.Status)
+		return
+	}
+
+	// fmt.Printf("type of response body is: %T", response.Body)
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Couldn't read :", err)
+		return
+	}
+
+	events := []Event{}
+	err = json.Unmarshal(data, &events)
+	if err != nil {
+		fmt.Println("Unmarshal err:", err)
+		return
+	}
+
+	for _, e := range events {
+		message := eventFormatter(e)
+		if message == "" {
+			continue
+		}
+		fmt.Println("-", message)
+	}
+}
